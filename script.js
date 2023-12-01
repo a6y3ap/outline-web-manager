a6e71d9e841681f2bbd448ce56d5d7ea186f9e5b
Abuzar <5120850+a6y3ap@users.noreply.github.com>
2023-12-01 12:47:46 +0500

    updated the code

--- script.js
+++ script.js
@@ -1,76 +1,102 @@
-document.getElementById('json-form').addEventListener('submit', function(event) {
-	event.preventDefault();
-	const jsonText = document.getElementById('json-text').value;
+const jsf = document.getElementById('json-form');
+const uf = document.getElementById('url-form');
 
-	try {
-		const jsonData = JSON.parse(jsonText);
-		const apiUrl = jsonData.apiUrl;
+if (jsf) {
+	jsf.addEventListener('submit', function(event) {
+		event.preventDefault();
+		const jsonText = document.getElementById('json-text').value;
 
-		if (apiUrl) {
-			// Fetch the JSON data from the extracted URL
-			fetch(apiUrl)
-				.then(response => response.json())
-				.then(data => {
-					const accessKeys = data.accessKeys;
-					const container = document.getElementById('json-display');
-					container.innerHTML = ''; // Clear previous data
+		try {
+			const jsonData = JSON.parse(jsonText);
+			const apiUrl = jsonData.apiUrl + '/access-keys/';
 
-					// Loop through the accessKeys and display the items
-					accessKeys.forEach(accessKey => {
+			if (apiUrl) {
+				// Fetch the JSON data from the extracted URL
+				fetch(apiUrl)
+					.then(response => response.json())
+					.then(data => {
+						const accessKeys = data.accessKeys;
+						const container = document.getElementById('json-display');
+						container.innerHTML = ''; // Clear previous data
+				
+						/* 
+						
+						// sending a post request to /access-keys/ generates a new key
 						const itemDiv = document.createElement('div');
-						itemDiv.innerHTML = `<p>ID: ${accessKey.id}</p>
-                                             <p>Password: ${accessKey.password}</p>
-                                             <p>Port: ${accessKey.port}</p>
-                                             <p>Method: ${accessKey.method}</p>
-                                             <p>AccessUrl: <span>${accessKey.accessUrl}</span> <button onclick="copyToClipboard('${accessKey.accessUrl}')">Copy</button></p>`;
+						itemDiv.innerHTML = `<form id="addKey" method="post" action="${jsonUrl}"><button type="button" onclick="addKey()">Add New Key</button></form>`;
+						itemDiv.classList.add('addKey');
 						container.appendChild(itemDiv);
+						
+						*/
+
+						// Loop through the accessKeys and display the items
+						accessKeys.forEach(accessKey => {
+							const itemDiv = document.createElement('div');
+							itemDiv.innerHTML = `<p>ID: ${accessKey.id}</p>
+												 <p>Password: ${accessKey.password}</p>
+												 <p>Port: ${accessKey.port}</p>
+												 <p>Method: ${accessKey.method}</p>
+												 <p>AccessUrl: <span>${accessKey.accessUrl}</span> <button onclick="copyToClipboard('${accessKey.accessUrl}')">Copy</button></p>`;
+							container.appendChild(itemDiv);
+						});
+					})
+					.catch(error => {
+						const container = document.getElementById('json-display');
+						container.innerHTML = '<p>Error fetching JSON data</p>';
+						console.error('Error fetching JSON:', error);
 					});
-				})
-				.catch(error => {
-					const container = document.getElementById('json-display');
-					container.innerHTML = '<p>Error fetching JSON data</p>';
-					console.error('Error fetching JSON:', error);
-				});
-		} else {
+			} else {
+				const container = document.getElementById('json-display');
+				container.innerHTML = '<p>No apiUrl found in the JSON data</p>';
+			}
+		} catch (error) {
 			const container = document.getElementById('json-display');
-			container.innerHTML = '<p>No apiUrl found in the JSON data</p>';
+			container.innerHTML = '<p>Invalid JSON format</p>';
+			console.error('Error parsing JSON:', error);
 		}
-	} catch (error) {
-		const container = document.getElementById('json-display');
-		container.innerHTML = '<p>Invalid JSON format</p>';
-		console.error('Error parsing JSON:', error);
-	}
-});
+	});
+}
+if (uf) {
+	uf.addEventListener('submit', function(event) {
+		event.preventDefault();
+		const jsonUrl = document.getElementById('json-url').value + '/access-keys/';
 
-document.getElementById('url-form').addEventListener('submit', function(event) {
-	event.preventDefault();
-	const jsonUrl = document.getElementById('json-url').value;
-
-	// Fetch the JSON data from the input URL
-	fetch(jsonUrl)
-		.then(response => response.json())
-		.then(data => {
-			const accessKeys = data.accessKeys;
-			const container = document.getElementById('json-display');
-			container.innerHTML = ''; // Clear previous data
-
-			// Loop through the accessKeys and display the items
-			accessKeys.forEach(accessKey => {
+		// Fetch the JSON data from the input URL
+		fetch(jsonUrl)
+			.then(response => response.json())
+			.then(data => {
+				const accessKeys = data.accessKeys;
+				const container = document.getElementById('json-display');
+				container.innerHTML = ''; // Clear previous data
+				
+				/* 
+				
+				// sending a post request to /access-keys/ generates a new key
 				const itemDiv = document.createElement('div');
-				itemDiv.innerHTML = `<p>ID: ${accessKey.id}</p>
-                                     <p>Password: ${accessKey.password}</p>
-                                     <p>Port: ${accessKey.port}</p>
-                                     <p>Method: ${accessKey.method}</p>
-                                     <p>AccessUrl: <span>${accessKey.accessUrl}</span> <button onclick="copyToClipboard('${accessKey.accessUrl}')">Copy</button></p>`;
+				itemDiv.innerHTML = `<form id="addKey" method="post" action="${jsonUrl}"><button type="button" onclick="addKey()">Add New Key</button></form>`;
+				itemDiv.classList.add('addKey');
 				container.appendChild(itemDiv);
+				
+				*/
+
+				// Loop through the accessKeys and display the items
+				accessKeys.forEach(accessKey => {
+					const itemDiv = document.createElement('div');
+					itemDiv.innerHTML = `<p>ID: ${accessKey.id}</p>
+										 <p>Password: ${accessKey.password}</p>
+										 <p>Port: ${accessKey.port}</p>
+										 <p>Method: ${accessKey.method}</p>
+										 <p>AccessUrl: <span>${accessKey.accessUrl}</span> <button onclick="copyToClipboard('${accessKey.accessUrl}')">Copy</button></p>`;
+					container.appendChild(itemDiv);
+				});
+			})
+			.catch(error => {
+				const container = document.getElementById('json-display');
+				container.innerHTML = '<p>Error fetching JSON data</p>';
+				console.error('Error fetching JSON:', error);
 			});
-		})
-		.catch(error => {
-			const container = document.getElementById('json-display');
-			container.innerHTML = '<p>Error fetching JSON data</p>';
-			console.error('Error fetching JSON:', error);
-		});
-});
+	});
+}
 
 // Function to copy the accessUrl to the clipboard
 function copyToClipboard(text) {
@@ -81,4 +107,17 @@ function copyToClipboard(text) {
 	document.execCommand('copy');
 	document.body.removeChild(el);
 	alert('Copied the text: ' + text);
+}
+
+// Function to add new access key
+function addKey() {
+	
+	// Access the form element
+	var form = document.getElementById('addKey');
+
+	// Submit the form
+	form.submit();
+
+	// Refresh the page after the form is submitted
+	window.location.reload();
 }
